25d9582854f3e2b208a3e2764958b28f4b0f62e6
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2021-09-18 17:32:12 -0300

    Detalhes Evento

--- src/main/java/com/eventoapp/repository/EventoRepository.java
+++ src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
 
+    Evento findByCodigo(long codigo);
+
 
 
 
